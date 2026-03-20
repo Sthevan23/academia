def8a63982360c45b53c92e4eac2be37f0565edb
Sthevan Welliton <sthevanicm@gmail.com>
2026-03-20 08:52:34 -0300

    alterando o site

--- script.js
+++ script.js
@@ -56,4 +56,26 @@
 
   updateCountdown();
   setInterval(updateCountdown, 1000);
+
+  var revealEls = document.querySelectorAll(".reveal");
+  if (revealEls.length && "IntersectionObserver" in window) {
+    var obs = new IntersectionObserver(
+      function (entries, o) {
+        entries.forEach(function (e) {
+          if (e.isIntersecting) {
+            e.target.classList.add("is-visible");
+            o.unobserve(e.target);
+          }
+        });
+      },
+      { rootMargin: "0px 0px -40px 0px", threshold: 0.08 }
+    );
+    revealEls.forEach(function (el) {
+      obs.observe(el);
+    });
+  } else {
+    revealEls.forEach(function (el) {
+      el.classList.add("is-visible");
+    });
+  }
 })();
